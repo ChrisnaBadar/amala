96de2ca2680f63402be33eb7e6318195a14126a3
ChrisnaBadar <62274044+ChrisnaBadar@users.noreply.github.com>
2023-02-28 16:07:19 +0700

    KNTR

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Amala());
+}
+
+class Amala extends StatefulWidget {
+  const Amala({super.key});
+
+  @override
+  State<Amala> createState() => _AmalaState();
+}
+
+class _AmalaState extends State<Amala> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
